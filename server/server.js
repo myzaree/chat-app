90eccfa5c27dd1a0525a325cbfaaaf34c9a209a7
Fuji <ray2freelife@gmail.com>
2019-03-13 17:19:42 +0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,9 +14,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected.');
 
+  socket.emit('newMessage', {
+    from: 'Admin', 
+    text: 'Welcome to the chat!',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin', 
+    ext: 'New user joined the chat.',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('Got message:', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
