aa44aa6d049627019d5337f097b2cc4e0444ca1d
Fuji <ray2freelife@gmail.com>
2019-03-13 13:26:29 +0600

    Edited port error

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
