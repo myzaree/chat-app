b2b2c866f94c149c98fee5dfa1112c5146d4dad5
Fuji <ray2freelife@gmail.com>
2019-03-13 13:15:51 +0600

    Emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -13,13 +13,20 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected.');
-  
+
+  socket.on('createMessage', (message) => {
+    console.log('Got message:', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected.');
   });
 });
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
