c55588ee1cd159052ea78f2658c0f8f1f9eece32
Fuji <ray2freelife@gmail.com>
2019-03-14 16:06:53 +0600

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ socket.on('newMessage', function(message) {
 socket.on('newLocationMessage', function(message) {
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(message.from);
+  li.text(`${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#chat-window').append(li);
@@ -26,11 +26,14 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(event){
   event.preventDefault();
+  var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: jQuery('[name=userName]').val(),
-    text: jQuery('[name=message]').val()
-  }, function(){});
+    text: messageTextbox.val()
+  }, function(){
+    messageTextbox.val('');
+  });
 });
 
 var locationButton = jQuery('#send-location');
@@ -39,14 +42,17 @@ locationButton.on('click', function(){
   if(!navigator.geolocation){
     return alert('Geolocation is not supported by your browser.');
   }
+  locationButton.attr('disabled', true).text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
 
   }, function(){
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to share location.');
   });
 });
